f1ae9995a345b72fc7c20267c97bb280a3f6c2b5
Jai Bhasin <bhasinjai@gmail.com>
2025-04-09 21:34:49 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,37 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+# from browser_use import Agent
+from pydantic import SecretStr
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+api_key = os.getenv("GEMINI_API_KEY")
+
+
+from browser_use import Agent, Browser, BrowserConfig
+import asyncio
+# Configure the browser to connect to your Chrome instance
+browser = Browser(
+    config=BrowserConfig(
+        # Specify the path to your Chrome executable
+        chrome_instance_path='C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',  # macOS path
+        # For Windows, typically: 
+        # For Linux, typically: '/usr/bin/google-chrome'
+    )
+)
+
+# llm = ChatOpenAI(model="gpt-4o")
+llm = ChatGoogleGenerativeAI(model='gemini-2.0-flash-exp', api_key=SecretStr(os.getenv('GEMINI_API_KEY')))
+
+
+async def main():
+    agent = Agent(
+    task="search for bhasin group india and give me emails of the top 5 people",
+    llm=llm,
+    browser=browser
+    )
+    result = await agent.run()
+    print(result)
+    await browser.close()
+
+asyncio.run(main())
